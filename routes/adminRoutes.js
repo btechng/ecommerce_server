df7629f095bfdee4641503634432c2e3a11eeb8f
btechng <btechngng@gmail.com>
2025-08-07 20:50:51 +0100

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../models/User.js";
+import Wallet from "../models/Wallet.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
